1e388283ffbbd62e5c3011518793c6c235c275b2
Sara Foresti <Sara.foresti@hotmail.com>
2018-05-15 11:51:58 +0100

    Added model and Tasks Controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  # Read all tasks
+
+  # Read one task
+
+  # Create one task (2 routes: one to show the form to the user, one to actually post the new instance???)
+
+  # Update one task (2 routes: same reasons as above?)
+
+  # Delete one task
 end
